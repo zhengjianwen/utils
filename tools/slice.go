e3b351aa9bf4c1ed76709884990c8987f18ba83c
HaiRui <zhengjianwen@users.noreply.github.com>
2018-11-18 10:23:43 +0800

    hairui
    排序

---
+++ tools/slice.go
@@ -0,0 +1,20 @@
+package tools
+
+import "sort"
+
+func SortSilceInt64(data []int64) []int64 {
+	
+	return data
+}
+
+
+func SortSilceInt(data []int) []int {
+	sort.Ints(data)
+	return data
+}
+
+
+func SortStr(data []string) []string {
+	sort.Strings(data)
+	return data
+}
