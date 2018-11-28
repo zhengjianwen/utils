de07ff49cff64c06fe38431c66367e0e28066610
HaiRui <zhengjianwen@users.noreply.github.com>
2018-11-28 18:00:11 +0800

    hairui
    add 切片排序

--- tools/slice.go
+++ tools/slice.go
@@ -3,16 +3,22 @@ package tools
 import "sort"
 
 func SortSilceInt64(data []int64) []int64 {
-	
-	return data
+	tmp := make([]int,0)
+	for _,v := range data{
+		tmp = append(tmp,int(v))
+	}
+	sort.Ints(tmp)
+	ret := make([]int64,0)
+	for _,v := range tmp{
+		ret = append(ret,int64(v))
+	}
+	return ret
 }
-
 
 func SortSilceInt(data []int) []int {
 	sort.Ints(data)
 	return data
 }
-
 
 func SortStr(data []string) []string {
 	sort.Strings(data)
